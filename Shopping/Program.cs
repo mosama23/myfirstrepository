cb2eb506604aab7c5ba717d64d131a04899f889a
osama <osama.virgoutlook.com>
2022-12-06 13:50:17 +0100

    initial commit

---
+++ Shopping/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Zwiebel");
+Console.WriteLine("Nudelblätter");
+Console.WriteLine("Tomaten");
+Console.WriteLine("Käse");
+Console.WriteLine("Fleisch");
+Console.WriteLine("Milch");
+Console.WriteLine("Butter");
+Console.WriteLine("Mehl");
+Console.WriteLine("Tomatopaste");
+Console.WriteLine("Öl");
+Console.WriteLine("Eggs");
+Console.WriteLine("tea");
+Console.WriteLine("knoblauch");
+Console.WriteLine("tiramisu");
+Console.WriteLine("weißwein");
+Console.WriteLine("Kaffee");
